aa5ce1c9c4f9e2633383e63d4d7b5cce70608ae5
Abhishek Vinjamoori <abhishek.vinjamoori@students.iiit.ac.in>
2016-06-24 00:44:47 +0530

    Login trials with urllib

---
+++ loginamazon.py
@@ -0,0 +1,25 @@
+import http.cookiejar
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
+
+amazon_username = "i@gmail.com"
+amazon_password = "32refdvfdgre"
+
+login_data = urllib.parse.urlencode({'action': 'sign-in',
+                               'email': amazon_username,
+                               'password': amazon_password,
+                               })
+
+binary_data = login_data.encode("utf-8")
+cookie = http.cookiejar.CookieJar()    
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')]
+
+response = opener.open('https://www.amazon.com/gp/sign-in.html')
+#print((response.getcode()))
+
+response = opener.open('https://www.amazon.com/gp/flex/sign-in/select.html', binary_data)
+#print((response.getcode()))
+
+response = opener.open("https://www.amazon.com/") # it should show that you are logged in
+print((response.read().decode("utf8")))
