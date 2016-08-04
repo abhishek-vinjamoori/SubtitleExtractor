d35d05f071a7ebeafe9a7a946de58bfcfde31147
Abhishek Vinjamoori <abhishek.vinjamoori@students.iiit.ac.in>
2016-08-05 01:56:42 +0530

    CrunchyRoll XML to SRT convertor

---
+++ CrunchyRoll_XmlToSrt.py
@@ -0,0 +1,52 @@
+import re
+import sys
+from bs4 import BeautifulSoup
+
+
+def toSrt(xml_string):
+	srt = ''
+	texts             = BeautifulSoup(xml_string)
+	listOfTranscripts = texts.findAll("event")
+
+	# TODO parse xml instead of regex
+	
+	captionNumber = 1
+	for captions in listOfTranscripts:
+		
+
+		start = captions['start']
+		end   = captions['end']
+		
+		start = formatTime(start)
+		end = formatTime(end)
+
+		caption = captions['text']
+		# caption = caption.replace('&#39;', "'")
+		# caption = caption.replace('&quot;', '"')
+		
+		srt += str(captionNumber) + '\n'
+		srt += start + ' --> ' + end + '\n'
+		srt += caption + '\n\n'
+
+		captionNumber += 1
+
+	return srt
+
+def formatTime(time):
+	
+
+	pieces    = time.split(".")
+	pieces[1] = (pieces[1]+"0"*3)[0:3]
+	formatted = "%s,%s" % (pieces[0],pieces[1])
+
+	return formatted
+
+def main():
+	f = open(sys.argv[1],"r")
+	q = f.read()
+	s = toSrt(q)
+	print(s)
+
+
+if __name__ == "__main__":
+	main()
