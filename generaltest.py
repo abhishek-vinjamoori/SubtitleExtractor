1d4eedf436d7ce0047e423b5272c6e8946305489
Abhishek Vinjamoori <abhishek.vinjamoori@students.iiit.ac.in>
2016-08-15 18:44:20 +0530

    Improved the failure results.

--- generaltest.py
+++ generaltest.py
@@ -1,23 +1,43 @@
+#! /usr/bin/env python3
+
+import sys
 from SubtitleExtractor import Subtitle
-from HuluTest import testList
+from HuluTest import HuluTestList
+from BbcTest import BbcTestList
 
-Extractor = Subtitle()
 
-counter = 0
-failureList = []
+def main():
 
-for entities in testList:
+	testList = HuluTestList
+	Extractor = Subtitle()
+
+	counter = 0
+	failureList = []
+
+	for entities in testList:
+		
+		url = entities[0]
+		Extractor.getServiceName(url)
+		Extractor.testMode = True
+		# if len(entities) == 5:
+		returnValue = Extractor.serviceProcess()
+
+		if returnValue:
+			print("File %d successful"%(counter+1))
+		else:
+			print("Test Failed on File - %d"%(counter+1))
+			failureList.append([counter+1,url])
+
+		counter += 1
+
+	print("\n\n\n------------------------------------------------------------------------\n\n")
 	
-	url = entities[0]
-	Extractor.getServiceName(url)
-	Extractor.testMode = True
-	# if len(entities) == 5:
-	returnValue = Extractor.serviceProcess()
+	if len(failureList):
+		print("Test failed on files -")
+		
+		for i in failureList:
+			print("<%d> - URL : %s" %(i[0],i[1]))
 
-	if returnValue:
-		print("File %d successful"%(counter+1))
-	else:
-		print("Test Failed on File - %d"%(counter+1))
-		failureList.append(counter+1,url)
 
-	counter += 1
+if __name__ == "__main__":
+	main()
