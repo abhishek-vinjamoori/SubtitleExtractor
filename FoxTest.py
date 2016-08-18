0677d1fe23547230e0b9bf4f8077c1635accaef8
Abhishek Vinjamoori <abhishek.vinjamoori@students.iiit.ac.in>
2016-08-18 13:59:40 +0530

    Fox test files

---
+++ FoxTest.py
@@ -0,0 +1,14 @@
+FoxTestList = [
+["http://www.fox.com/watch/677094979888/7683748608" ] ,
+["http://www.fox.com/watch/681382467805/7683748608" ] ,
+["http://www.fox.com/watch/684171331973/7684520448" ] ,
+["http://www.fox.com/watch/711899203521/7809362688" ] ,
+["http://www.fox.com/watch/255180355939/7684182528" ] ,
+["http://www.fox.com/watch/576384579574/7766135808" ] ,
+["http://www.fox.com/watch/690945603630/7684182528" ] ,
+["http://www.fox.com/watch/660599363942/7684159488" ] ,
+["http://www.fox.com/watch/688407619776/7684647168" ] ,
+["http://www.fox.com/watch/692180035973/7765713408" ] ,
+["http://www.fox.com/watch/707325507508/7683679488" ] ,
+["http://www.fox.com/watch/660599363942/7684159488" ] ,
+]
